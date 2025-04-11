0ef1311ca7fdfc28b9d38eb12b6fb5a86af23579
Joan Soldevila Villalba <joansoldevilarosellmendez@gmail.com>
2025-04-11 18:01:24 +0200

    Update Tauler.cpp

--- Tauler.cpp
+++ Tauler.cpp
@@ -1,1 +1,17 @@
+#include "Tauler.h"
+void Tauler::inicialitza(const string& nomFitxer){
+    
+}
+void Tauler::actualitzaMovimentsValids(){
+    
+}
+void Tauler::getPosicionsPossibles(const Posicio& origen, int& nPosicions, Posicio posicionsPossibles[]){
+    
+}
+bool Tauler::mouFitxa(const Posicio& origen, const Posicio& desti){
+    
+}
+  string toString() const{
+}
 
+#endif
