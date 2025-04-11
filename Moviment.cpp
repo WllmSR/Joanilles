e3b1f1b2bd0b8627aaa6285107ca7be39810b626
Joan Soldevila Villalba <joansoldevilarosellmendez@gmail.com>
2025-04-11 17:53:00 +0200

    Update Moviment.cpp

--- Moviment.cpp
+++ Moviment.cpp
@@ -1,1 +1,25 @@
+#include "Moviment.h"
+Moviment::Moviment(){
+  
+}
+void Moviment::afegeixPas(const Posicio& p){
+  
+}              // Afegir pas del moviment
+void Moviment::afegeixCaptura(const Posicio& p){
+  
+}          // Afegir una peça capturada
+const vector<Posicio>& Moviment::getRecorregut() const{
+  
+}
+const vector<Posicio>& Moviment::getCaptures() const{
+  
+}
+Posicio Moviment::getDestiFinal() const{
+  
+}
+int Moviment::numCaptures() const{
+}
+bool Moviment::esCaptura() const{
+}
 
+#endif
