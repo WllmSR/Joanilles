44a4ee3fe947754ec2772a2d1e4c849164ae6a88
Joan Soldevila Villalba <joansoldevilarosellmendez@gmail.com>
2025-04-11 17:58:58 +0200

    Update Posicio.cpp

--- Posicio.cpp
+++ Posicio.cpp
@@ -1,1 +1,18 @@
+Posicio::Posicio(){
+}
+Posicio::Posicio(const string& pos){
+  
+}
+int Posicio::getFila() const{
+return fila;      
+}
+int Posicio::getColumna() const{
+      return columna;
+}
+string Posicio::toString() const{
+      
+}
+bool Posicio::operator==(const Posicio& other) const{
+  return ((fila==other.fila)&&(columna==other.columna));
+}
 
