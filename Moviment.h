b3e891b78422ae59031f2c085a2f3c1037056a71
joanSoldevila <163576469+joanSoldevila@users.noreply.github.com>
2025-04-26 18:09:44 +0200

    Update Moviment.h

--- Moviment.h
+++ Moviment.h
@@ -11,6 +11,8 @@ private:
     vector<Posicio> pecesCapturades;  //podem ferho amb la classe fitxa???      podem afegir un metode que ho revisi
     //int+constructor per guardar el nombre de captures o només metode???
 
+//hauriem de modificar vector<Posicio> recorregut i vector<Posicio> pecesCapturades per : vector<vector<Fitxa>>m_recorregut vector<vector<Fitxa>> m_capturades (Fitxa==posicio basicament)
+
 public:
     Moviment();
 
@@ -20,7 +22,8 @@ public:
     const vector<Posicio>& getCaptures() const;
 //la funcio mirarCapturesDretaEsquerra, s'haura de modificar una mica, els parametres no estan del tot correcta, pero l'estrucutra del codi està ben plantejat
 mirarCapturesDretaEsquerra(const Casella& origen, vector<Casella>& parametrePosicions, vector<pair<Casella, bool>>& parametreCaptures, const Casella m_tauler[][8])     Posicio getDestiFinal() const;
-    int numCaptures() const;
+void Moviments::DFS(const Fitxa m_tauler[][8], vector<vector<Fitxa>>& recorregut,vector<vector<Fitxa>>&pecesCaptured  const Fitxa& original);    
+int numCaptures() const;
 };
 
 #endif
