71dfcd9b4da0e568a46270089175b04e2776f2e6
WllmSR <167131601+WllmSR@users.noreply.github.com>
2025-04-27 19:41:23 +0200

    Update Posicio.h

--- Posicio.h
+++ Posicio.h
@@ -1,24 +1,26 @@
 #ifndef POSICIO_H
 #define POSICIO_H
 
+//IMPORTANT: les files van de 1 a 8, no de 0 a 7.
+
 #include <string>
 using namespace std;
 
-class Posicio {
+class Posicio
+{
 private:
     int fila;
     int columna;
 
 public:
-    Posicio();
+    Posicio() { fila = 0; columna = 0; }
     Posicio(const string& pos);
 
-    int getFila() const;
-    int getColumna() const;
-    //setters?
+    int getFila() const { return fila; }
+    int getColumna() const { return columna; }
 
-    string toString() const;
     bool operator==(const Posicio& other) const;
+    Posicio& operator=(const Posicio& other)
 };
 
 #endif
