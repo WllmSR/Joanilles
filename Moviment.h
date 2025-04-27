5ac450685cb147bf65bd2b979a3efc985f3f3297
Joan Soldevila Villalba <joansoldevilarosellmendez@gmail.com>
2025-04-27 20:30:30 +0200

    Update Moviment.h

--- Moviment.h
+++ Moviment.h
@@ -1,34 +1,30 @@
 #ifndef MOVIMENT_H
 #define MOVIMENT_H
-
 #include <vector>
-#include "posicio.h"
+#include <tuple>
+#include <utility> // per pair
+#include <algorithm> //per find, m'ha fet pal escriure mes codi
+#include <string>
+#include <stack>
+#include "Posicio.h"
 using namespace std;
-
 class Moviment {
 private:
     vector<vector<Fitxa>> recorregut;
-    vector<vector<Fitxa>> pecesCapturades;  //podem ferho amb la classe fitxa???      podem afegir un metode que ho revisi
-    //int+constructor per guardar el nombre de captures o només metode???
-
-//hauriem de modificar vector<Posicio> recorregut i vector<Posicio> pecesCapturades per : vector<vector<Fitxa>>m_recorregut vector<vector<Fitxa>> m_capturades (Fitxa==posicio basicament)
-
+    vector<vector<Fitxa>> pecesCapturades;
 public:
     Moviment();
-
+    ~Moviment();
     void afegeixPas(const Posicio& p);      //un sol metode?? constructor?
     void afegeixCaptura(const Posicio& p);     
-    const vector<Posicio>& getRecorregut() const;
-    const vector<Posicio>& getCaptures() const;
-//la funcio mirarCapturesDretaEsquerra, s'haura de modificar una mica, els parametres no estan del tot correcta, pero l'estrucutra del codi està ben plantejat
-mirarCapturesDretaEsquerra(const Casella& origen, vector<Casella>& parametrePosicions, vector<pair<Casella, bool>>& parametreCaptures, const Casella m_tauler[][8])     Posicio getDestiFinal() const;
-void Moviments::DFS(const Fitxa m_tauler[][8], vector<vector<Fitxa>>& recorregut,vector<vector<Fitxa>>&pecesCaptured  const Fitxa& original);    
-//segons el que s'ha dit anteriorment, la funcio numCaptures retorna les captures d'un camí en concret, de manera que li hem de passar un index com a parametre per retornar el numero de captures d'un camí en concret
+    const vector<Posicio>& getRecorregut(int posicioIndex) const;
+    const vector<Posicio>& getCaptures(int posicioIndex) const;
+void mirarCapturesDretaEsquerra(const Fitxa& origen, vector<Fitxa>& parametrePosicions, vector<pair<Fitxa, bool>>& parametreCaptures, const Fitxa m_tauler[][8])     Posicio getDestiFinal() const;
+void DFSDames(const Fitxa m_tauler[][8], vector<vector<Fitxa>>& recorregut,vector<vector<Fitxa>>&pecesCaptured , const Fitxa& original);    
+void DFSNormal(const Fitxa m_tauler[][8], vector<vector<Fitxa>>& recorregut,vector<vector<Fitxa>>&pecesCaptured , const Fitxa& original); //aquesta funcio es la que s'ha de cridar per fer el DFS, i es la que s'ha de modificar per fer el DFS normal
 int numCaptures(int index) const;
-//int numCapturesTotal()const es una funcio que retorna la suma de totes les peces que podem capturar de tots els camins possibles
 int numCapturesTotal()const;
-void mirarCapturesDames(const Fitxa& orifen, vector<Fitxa>& parametrePosicions, vector<pair<Fitxa, bool>>& parametreCaptures, const Fitxa m_taules[][8]){
-
+void mirarCapturesDames(const Fitxa& origen,vector<Fitxa>& parametrePosicionsDreta,vector<Fitxa>& parametrePosicionsEsquerra, vector<pair<Fitxa, bool>>& parametreCapturesDreta,vector<pair<Fitxa, bool>>& parametreCapturadesEsquerra, const Fitxa m_taules[][9]); 
 };
 
 #endif
