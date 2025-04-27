619c740f728aef86e496b91feb01063c2d2c56e5
WllmSR <167131601+WllmSR@users.noreply.github.com>
2025-04-27 19:41:46 +0200

    Update Posicio.cpp

--- Posicio.cpp
+++ Posicio.cpp
@@ -1,19 +1,21 @@
 #include "Posicio.h"
-Posicio::Posicio(){
+
+Posicio::Posicio(const array<char, 2>& pos)
+{
+  fila = pos[1] - '0';
+  columna = pos[0] - 'a' + 1;  
 }
-Posicio::Posicio(const string& pos){
-  
-}
-int Posicio::getFila() const{
-return fila;      
-}
-int Posicio::getColumna() const{
-      return columna;
-}
-string Posicio::toString() const{
-      
-}
-bool Posicio::operator==(const Posicio& other) const{
+
+bool Posicio::operator==(const Posicio& other) const
+{
   return ((fila==other.fila)&&(columna==other.columna));
 }
 
+Posicio& Posicio::operator=(const Posicio& other)
+{
+  {
+      fila = other.fila;
+      columna = other.columna;
+  }
+  return *this;    //punter GOD
+}
