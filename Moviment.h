abf358c13c2687ce2a418fa497f85e4930ad2abf
joanSoldevila <163576469+joanSoldevila@users.noreply.github.com>
2025-04-29 21:00:16 +0200

    Update Moviment.h

--- Moviment.h
+++ Moviment.h
@@ -7,27 +7,33 @@
 #include <string>
 #include <stack>//fem ús de l'algorisme dfs, per tant es necessari utilitzar un stack, ho podem fer recursivament  tambe
 #include "Posicio.h"
+#include "Fitxa.h"
 using namespace std;
 class Moviment {
 private:
     vector<vector<Posicio>> recorregut;
     vector<vector<Posicio>> pecesCapturades;
-    vector<Posicio> destins
+    vector<Posicio> destins;
 public:
     Moviment();
     ~Moviment();
-    //void afegeixPas(const Posicio& p);      //un sol metode?? constructor?
-    //void afegeixCaptura(const Posicio& p);     
-    const vector<Posicio>& getRecorregut(int posicioIndex) const;
-    const vector<Posicio>& getCaptures(int posicioIndex) const;
-void mirarCapturesDretaEsquerra(const Fitxa& origen, vector<Fitxa>& parametrePosicions, vector<pair<Fitxa, bool>>& parametreCaptures, const Fitxa m_tauler[][8])     Posicio getDestiFinal() const;
-void DFSDames(const Fitxa m_tauler[][8], vector<vector<Fitxa>>& recorregut,vector<vector<Fitxa>>&pecesCaptured , const Fitxa& original);    
-void DFSNormal(const Fitxa m_tauler[][8], vector<vector<Fitxa>>& recorregut,vector<vector<Fitxa>>&pecesCaptured , const Fitxa& original); //aquesta funcio es la que s'ha de cridar per fer el DFS, i es la que s'ha de modificar per fer el DFS normal
+  const vector<Posicio>& Moviment::getRecorregut(int posicioIndex) const{
+return recorregut[posicioIndex];
+}
+const vector<Posicio>& Moviment::getCaptures(int posicioIndex) const{
+return pecesCapturades[posicioIndex]; 
+}
+void mirarCapturesDretaEsquerra(const Posicio& origen, vector<pair<Posicio, int>>& parametrePosicions, vector<pair<Posicio, bool>>& parametreCaptures, const Fitxa m_tauler[][9]);
+
+Posicio getDestiFinal() const;
+void DFSDames(const Fitxa m_tauler[][9], vector<vector<Posicio>>& recorregut,vector<vector<Posicio>>&pecesCaptured , const Posicio& original);    
+void DFSNormal(const Fitxa m_tauler[][9], vector<vector<Posicio>>& recorregut,vector<vector<Posicio>>&pecesCaptured , const Posicio& original); //aquesta funcio es la que s'ha de cridar per fer el DFS, i es la que s'ha de modificar per fer el DFS normal
 int numCaptures(int index) const;
 int numCapturesTotal()const;
-void mirarCapturesDames(const Fitxa& origen,vector<Fitxa>& parametrePosicionsDreta,vector<Fitxa>& parametrePosicionsEsquerra, vector<pair<Fitxa, bool>>& parametreCapturesDreta,vector<pair<Fitxa, bool>>& parametreCapturadesEsquerra, const Fitxa m_taules[][9]); 
-void actualitzaMoviments(const Fitxa m_tauler[][8], const Fitxa& original, vector<Fitxa>& m_posicionsPossibles, vector<Fitxa>& m_posicionsCapturades);
+void mirarCapturesDames(int AmuntBaix,int continuarDreta, int continuarEsquerra,const Posicio& origen,vector<Posicio>& parametrePosicionsDreta,vector<Posicio>& parametrePosicionsEsquerra, pair<Posicio, bool>& parametreCapturesDreta,pair<Posicio, bool>& parametreCapturadesEsquerra, const Fitxa m_taules[][9]); 
 
+void actualitzaMoviments(const Fitxa m_tauler[][9], const Posicio& original, vector<Posicio>& m_posicionsPossibles, vector<Posicio>& m_posicionsCapturades);
+
+bool esValid(int fila, int columna)const{return (fila>=0&&fila<=8)&&(columna>=0&&columna<=8);}
 };
-
 #endif
