d1cb7d62451d6283e527ffa45544eb87350791b3
Joan Soldevila Villalba <joansoldevilarosellmendez@gmail.com>
2025-05-08 14:24:19 +0200

    Update Posicio.h

--- include/Posicio.h
+++ include/Posicio.h
@@ -1,19 +1,19 @@
 #pragma once
 #ifndef POSICIO_H
 #define POSICIO_H
+
 #include <array>
 
-class Posicio
-{
+class Posicio {
 private:
     int fila;
     int columna;
 
 public:
-    Posicio() { fila = 0; columna = 0; }
+    Posicio() : fila(0), columna(0) {}
     Posicio(const std::array<char, 2>& pos);
-    void inicialitza(int fill, int coll) { fila = fill; columna = coll; }
-    Posicio(int Nfila, int Ncolumna) { fila = Nfila; columna = Ncolumna; }
+    Posicio(int Nfila, int Ncolumna) : fila(Nfila), columna(Ncolumna) {}
+
     int getFila() const { return fila; }
     int getColumna() const { return columna; }
 
