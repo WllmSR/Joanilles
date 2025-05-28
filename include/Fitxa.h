e654400e037766f4fb77ade7c19649d6babb917a
Joan Soldevila Villalba <joansoldevilarosellmendez@gmail.com>
2025-05-28 19:58:53 +0200

    Update Fitxa.h

--- include/Fitxa.h
+++ include/Fitxa.h
@@ -1,35 +1,51 @@
 #pragma once
-#include "Fitxa.h"
+#ifndef FITXA_H
+#define FITXA_H
 
-#include <string>
-#include <fstream>
 #include <array>
-#include <iostream>
-#define N_FILES 8
-#define N_COLUMNES 8
-using namespace std;
-class Tauler {
-private:
+#include "Posicio.h"
+#include "Moviment.h"
 
-    Fitxa m_tauler[N_FILES][N_COLUMNES];
+enum TipusFitxa {
+    TIPUS_EMPTY = -1,
+    TIPUS_NORMAL = 0,
+    TIPUS_DAMA = 1
+};
+
+enum ColorFitxa {
+    COLOR_NEGRE = -1,
+    COLOR_BLANC = 1,
+    COLOR_EMPTY = 0
+};
+
+class Fitxa {
+private:
+    TipusFitxa tipus;
+    ColorFitxa color;
+    Posicio posFitxa;
+    Moviment moviments;
 
 public:
-    Tauler() {}
-    ~Tauler() {}
-    //void mostrarFormatSting()const;
-    void inicialitza(const std::string& nomFitxer);
-    std::string toString() const;
-    void getPosicionsPossibles(const Posicio& origen, int& nPosicions, Posicio posicionsPossibles[]);
-    void actualitzaMovimentsValids();
-    bool mouFitxa(const Posicio& origen, const Posicio& desti);
+    Fitxa() : tipus(TIPUS_EMPTY), color(COLOR_EMPTY), posFitxa() {}
+    Fitxa(char tipusIcolor, const std::array<char, 2>& pos);
 
-    int trobarDestifinal(std::vector<Posicio> burrito,const Posicio& valor)const;
-    void actualitzaTaulerNormal(int valor, std::vector<Posicio> camiActual);
-   // void actualitzaTaulerDames(int valor, std::vector<Posicio>camiActual);
-    void printTaulerComplert();
-    
-//    void printFacil();
-    };
+    TipusFitxa getTipus() const { return tipus; }
+    ColorFitxa getColor() const { return color; }
+    std::vector<Posicio> getMoviments() const { return moviments.getTotalDestins(); }
+    std::vector<std::vector<Posicio>> getCaptures()const { return moviments.getTotalCaptures();}
+    std::vector<std::vector<Posicio>> getCamins()const { return moviments.getTotalRecorregut(); }
+    Posicio getPos() const { return posFitxa; }
+    void setPos(int j, int i) { posFitxa.setColumna(j);posFitxa.setFila(i);}
 
+    void setTipus(TipusFitxa t) { tipus = static_cast<TipusFitxa>(t); }
+    void setColor(ColorFitxa c) { color = static_cast<ColorFitxa>(c); }
 
+    void netejaMoviments() { moviments.CleanMoviment(); }
+    void ferDama() { tipus = TIPUS_DAMA; }
+    bool esBuit() const { return tipus == TIPUS_EMPTY; }
+    bool esAmic(const Fitxa& variable) const { return color == variable.color; }
+    bool esEnemic(const Fitxa& variable) const { return color != variable.color; }
+    void calcularPosicionsPossibles(const Fitxa tauler[][8], const Posicio& original);
+};
 
+#endif
