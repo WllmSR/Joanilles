4ef26f1d51edbf0c3f0ada5533da97136e6b634c
Joan Soldevila Villalba <joansoldevilarosellmendez@gmail.com>
2025-05-28 19:57:45 +0200

    Update Posicio.cpp

--- source/Posicio.cpp
+++ source/Posicio.cpp
@@ -1,23 +1,15 @@
 #include "Posicio.h"
+#include <iostream>
 using namespace std;
 Posicio::Posicio(const array<char, 2>& pos)
 {
     fila = pos[1] - '0';
-    fila = fila - 1;
     columna = pos[0] - 'a' + 1;
     columna = columna - 1;
+    fila = fila - 1;
 }
-
 bool Posicio::operator==(const Posicio& other) const
 {
     return ((fila == other.fila) && (columna == other.columna));
 }
 
-Posicio& Posicio::operator=(const Posicio& other)
-{
-
-    fila = other.fila;
-    columna = other.columna;
-
-    return *this;    //punter GOD
-}
