b2afea6e8d663c065fc64680417c03b4ce7b79d8
Joan Soldevila Villalba <joansoldevilarosellmendez@gmail.com>
2025-05-28 19:56:41 +0200

    Update Fitxa.cpp

--- source/Fitxa.cpp
+++ source/Fitxa.cpp
@@ -1,6 +1,7 @@
 #include "Fitxa.h"
-
-Fitxa::Fitxa(char tipusIcolor, const std::array<char, 2>& pos, const Fitxa tauler[][8]):posFitxa(pos)
+using namespace std;
+#include <iostream>
+Fitxa::Fitxa(char tipusIcolor, const std::array<char, 2>& pos):posFitxa(pos)
 {
     switch (tipusIcolor)
     {
@@ -28,14 +29,11 @@ Fitxa::Fitxa(char tipusIcolor, const std::array<char, 2>& pos, const Fitxa taule
 
 
 }
-void Fitxa::calcularPosicionsPossibles(const Fitxa tauler[][8], const Posicio& original) {
-    //aqui es on traduiem la fitxa tauler a un int fauler
+void Fitxa::calcularPosicionsPossibles(const Fitxa tauler[][8], const Posicio& original){
     std::vector<std::vector<std::pair<int, int>>> taulerTraduir;
     taulerTraduir.resize(8, std::vector<std::pair<int,int>>(8));
-    // Copy data from array to vector
-    //fem que el primer va el tipus, el segon es el color.
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
+   for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
             taulerTraduir[i][j].first = static_cast<int>(tauler[i][j].getTipus());
             taulerTraduir[i][j].second = static_cast<int>(tauler[i][j].getColor());
         }
