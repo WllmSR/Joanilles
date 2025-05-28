ae3eec8fdb848ddca6782a18012d3903e8d06e78
Joan Soldevila Villalba <joansoldevilarosellmendez@gmail.com>
2025-05-28 19:59:34 +0200

    Update Posicio.h

--- include/Posicio.h
+++ include/Posicio.h
@@ -3,6 +3,8 @@
 #define POSICIO_H
 
 #include <array>
+#include <string>
+#include <ostream>
 
 class Posicio {
 private:
@@ -12,13 +14,35 @@ private:
 public:
     Posicio() : fila(0), columna(0) {}
     Posicio(const std::array<char, 2>& pos);
+
+    Posicio(const std::string& hola) {
+        fila = hola[1] - '0';
+        columna = hola[0] - 'a';
+        fila = fila - 1;
+    }
+
     Posicio(int Nfila, int Ncolumna) : fila(Nfila), columna(Ncolumna) {}
+
+    void setFila(int f) { fila = f; }
+    void setColumna(int c) { columna = c; }
 
     int getFila() const { return fila; }
     int getColumna() const { return columna; }
 
     bool operator==(const Posicio& other) const;
-    Posicio& operator=(const Posicio& other);
+    Posicio& operator=(const Posicio& other) {
+        if (this != &other) {
+            fila = other.fila;
+            columna = other.columna;
+        }
+        return *this;
+    }
+
+    friend std::ostream& operator<<(std::ostream& output, const Posicio& temporal) {
+        output << "fila: " << temporal.fila << ", columna: " << temporal.columna << std::endl;
+        return output;
+    }
 };
 
 #endif
+
